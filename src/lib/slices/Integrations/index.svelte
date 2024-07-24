133dabdf8cf8c11c8acc9030560be075d4bf8ad0
Daniel Burgess <dan-burgess@hotmail.com>
2024-07-24 19:26:10 +0930

    animate integrations component

--- src/lib/slices/Integrations/index.svelte
+++ src/lib/slices/Integrations/index.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Content } from '@prismicio/client';
-
+	import { onMount } from 'svelte';
+	import gsap from 'gsap';
 	import background from './background.jpg';
 	import Bounded from '$lib/components/Bounded.svelte';
 	import LogoBackground from './LogoBackground.svelte';
@@ -24,9 +25,68 @@
 	};
 
 	export let slice: Content.IntegrationsSlice;
+
+	onMount(() => {
+		const tl = gsap.timeline({
+			repeat: -1,
+			defaults: { ease: 'power2.inOut' }
+		});
+
+		tl.to('.pulsing-logo', {
+			keyframes: [
+				{
+					filter: 'brightness(2)',
+					opacity: 1,
+					duration: 0.4,
+					ease: 'power2.in'
+				},
+				{ filter: 'brightness(1)', opacity: 0.7, duration: 0.9 }
+			]
+		});
+
+		tl.to(
+			'.signal-line',
+			{
+				keyframes: [
+					{ backgroundPosition: '0% 0%' },
+					{
+						backgroundPosition: '100% 100%',
+						stagger: { from: 'center', each: 0.3 },
+						duration: 1
+					}
+				]
+			},
+			'-=1.4'
+		);
+
+		tl.to('.pulsing-icon', {
+			keyframes: [
+				{
+					opacity: 1,
+          duration: 1,
+					stagger: {
+						from: 'center',
+						each: 0.3
+					},
+				},
+				{
+					opacity: .4,
+          duration: 1,
+					stagger: {
+						from: 'center',
+						each: 0.3
+					},
+				}
+			]
+		}, "-=2");
+	});
 </script>
 
-<Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} class="relative overflow-hidden">
+<Bounded
+	data-slice-type={slice.slice_type}
+	data-slice-variation={slice.variation}
+	class="relative overflow-hidden"
+>
 	<img src={background} alt="" class="absolute inset-0 h-full w-full object-cover" />
 	<LogoBackground />
 	<div class="relative">
